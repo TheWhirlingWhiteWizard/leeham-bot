c710922eabd788b219fe648f54c1b1b4924e3fe2
TheWhirlingWhiteWizard <30366211+TheWhirlingWhiteWizard@users.noreply.github.com>
2018-10-16 12:12:34 +1000

    Update accountdetails.py

--- accountdetails.py
+++ accountdetails.py
@@ -1,2 +1,2 @@
-details = dict(clientid='sIqEEPr8jjRblQ', clientsecret='DXEBpMvOYReGeoWdOULcyRgkfNw',
-               username='Leeham_Bot', password='6srZH0kO6lXIPs')
+details = dict(clientid='secret', clientsecret='secret',
+               username='secret', password='secret')
